ad2c84e4bfde247401b099d1251e511a28f46480
TarasiukDima <dimatarasiuk91@gmail.com>
2022-05-07 17:35:21 +0300

    refactor: add changes from notes

--- app/src/components/App/App.tsx
+++ app/src/components/App/App.tsx
@@ -1,18 +1,23 @@
 import React, { FC } from 'react';
 
+import { CssBaseline } from '@mui/material';
 import Header from '$components/Header';
 import Main from '$components/Main';
+import AppRoutes from '$components/AppRoutes';
 import Footer from '$components/Footer';
 
 import './App.scss';
 
 const App: FC = () => {
   return (
-    <>
+    <React.Fragment>
+      <CssBaseline />
       <Header />
-      <Main />
+      <Main>
+        <AppRoutes />
+      </Main>
       <Footer />
-    </>
+    </React.Fragment>
   );
 };
 
