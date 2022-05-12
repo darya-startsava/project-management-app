d7f242fe19205d5da4a756b2434b946d755c4710
exekuta <exekuta@gmail.com>
2022-05-12 14:56:23 +0300

    feat: add authors avatars

--- app/src/pages/Welcome/Welcome.tsx
+++ app/src/pages/Welcome/Welcome.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { Grid, Button, Modal, Typography } from '@mui/material';
+import { Grid, Button, Modal, Typography, Avatar } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import youtubeBigSign from '../../assets/svg/youtubeBigSign.svg';
+import author1 from '../../assets/images/Dmitrij.jpg';
+import author2 from '../../assets/images/Andrey.jpg';
+import author3 from '../../assets/images/Darya.jpeg';
 
 import css from './Welcome.module.scss';
 
-//TODO: Заменить текст-заглушку "Об авторах" в модальном окне и вставить фотографии/аватары авторов
+//TODO: Заменить текст-заглушку "Об авторах" в модальном окне
 
 const Welcome = () => {
   const [showAuthors, setShowAuthors] = useState<boolean>(false);
@@ -73,24 +75,24 @@ const Welcome = () => {
           aria-describedby={t('Welcome.modalDescriptionAriaLabel')}
         >
           <Grid className={css.modal}>
-            <Typography variant="h6" component="h2" align="center" sx={{ marginBottom: '20px' }}>
+            <Typography variant="h6" component="h2" align="center" sx={{ mb: 6 }}>
               {t('Welcome.aboutUs')}
             </Typography>
             <Grid container sx={{ justifyContent: 'center', gap: '20px' }}>
               <Grid item className={css.developer} direction="column">
-                <PersonOutlineIcon />
-                <Typography>{t('Welcome.author1')}</Typography>
+                <Avatar alt={t('Welcome.author1')} src={author1} sx={{ width: 200, height: 200 }} />
+                <Typography sx={{ mt: 2 }}>{t('Welcome.author1')}</Typography>
               </Grid>
               <Grid item className={css.developer} direction="column">
-                <PersonOutlineIcon />
-                <Typography>{t('Welcome.author2')}</Typography>
+                <Avatar alt={t('Welcome.author2')} src={author2} sx={{ width: 200, height: 200 }} />
+                <Typography sx={{ mt: 2 }}>{t('Welcome.author2')}</Typography>
               </Grid>
               <Grid item className={css.developer} direction="column">
-                <PersonOutlineIcon />
-                <Typography>{t('Welcome.author3')}</Typography>
+                <Avatar alt={t('Welcome.author3')} src={author3} sx={{ width: 200, height: 200 }} />
+                <Typography sx={{ mt: 2 }}>{t('Welcome.author3')}</Typography>
               </Grid>
             </Grid>
-            <Typography sx={{ mt: 2 }} align="center">
+            <Typography sx={{ mt: 4 }} align="center">
               {t('Welcome.modalText')}
             </Typography>
           </Grid>
