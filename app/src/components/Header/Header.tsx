9a6a51efa23aed903ffadb09814df5a59e29dbeb
darya-startsava <daryastartsava@gmail.com>
2022-05-13 16:28:07 +0200

    feat: add heading and logo

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-import { AppBar, useScrollTrigger, Toolbar, Button, Grid } from '@mui/material';
+import { AppBar, useScrollTrigger, Toolbar, Button, Grid, Typography } from '@mui/material';
 import LanguageToggler from '$components/general/LanguageToggler';
 import { useTranslation } from 'react-i18next';
 import LogoutIcon from '@mui/icons-material/Logout';
@@ -11,6 +11,8 @@ import DeveloperBoardIcon from '@mui/icons-material/DeveloperBoard';
 import HomeIcon from '@mui/icons-material/Home';
 import { useAppSelector } from '$store/store';
 import { ROUTES_PATHS } from '$settings/routing';
+import css from './Header.module.scss';
+import KanbanLogo from '$assets/kanban-logo.png';
 
 const Header: FC = () => {
   const { token } = useAppSelector((state) => state.app);
@@ -20,7 +22,11 @@ const Header: FC = () => {
   return (
     <AppBar position="sticky" color={trigger ? 'primary' : 'inherit'}>
       <Toolbar>
-        <Grid container spacing={0} direction="row" alignItems="center" justifyContent="center">
+        <Typography className={css.header__heading} variant="h6" component="h1">
+          Kanban
+        </Typography>
+        <img className={css.header__logo} src={KanbanLogo} alt={t('Header.logo')} />
+        <Grid container spacing={0} direction="row" alignItems="center" justifyContent="end">
           <LanguageToggler />
           {token ? (
             <>
@@ -32,17 +38,17 @@ const Header: FC = () => {
                 {t('Header.editProfile')}
               </Button>
               <Button
+                sx={{ textTransform: 'none', color: '#001a33', fontSize: '16px' }}
+                startIcon={<DeveloperBoardIcon />}
+              >
+                {t('Header.newBoard')}
+              </Button>
+              <Button
                 href={ROUTES_PATHS.logout}
                 sx={{ textTransform: 'none', color: '#001a33', fontSize: '16px' }}
                 startIcon={<LogoutIcon />}
               >
                 {t('Header.signOut')}
-              </Button>
-              <Button
-                sx={{ textTransform: 'none', color: '#001a33', fontSize: '16px' }}
-                startIcon={<DeveloperBoardIcon />}
-              >
-                {t('Header.newBoard')}
               </Button>
               <Button
                 href={ROUTES_PATHS.welcome}
