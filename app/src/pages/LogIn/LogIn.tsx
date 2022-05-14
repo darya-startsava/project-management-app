7eecb547a38b5c870796136625aa846e79bedced
darya-startsava <daryastartsava@gmail.com>
2022-05-14 02:23:35 +0200

    feat: add login and registration form validation
    feat: add login and registration form validation

--- app/src/pages/LogIn/LogIn.tsx
+++ app/src/pages/LogIn/LogIn.tsx
@@ -3,18 +3,24 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Button, CircularProgress, TextField, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { ErrorMessage } from '@hookform/error-message';
 import { IUserLogIn } from '$types/common';
 import { setToken } from '$store/appSlice';
 import { useSignInMutation } from '$services/api';
 import Section from '$components/Section';
-import { useTranslation } from 'react-i18next';
 import { ROUTES_PATHS } from '$settings/routing';
+import css from './Login.module.scss';
 
 const LogIn: FC = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const { register, handleSubmit } = useForm<IUserLogIn>();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IUserLogIn>();
   const [signIn, { isLoading }] = useSignInMutation();
 
   const onSubmit: SubmitHandler<IUserLogIn> = async (data) => {
@@ -36,19 +42,26 @@ const LogIn: FC = () => {
     <Section pageAllSpace={true}>
       <Typography variant="h3">{t('LogIn.signInTitle')}</Typography>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {/* TODO: add validation */}
         <TextField
-          {...register('login')}
-          required
+          {...register('login', { required: t('LogIn.loginFieldRequiredMessage') })}
           label={t('LogIn.loginLabel')}
           placeholder={t('LogIn.loginPlaceholder')}
         />
+        <ErrorMessage
+          errors={errors}
+          name="login"
+          render={({ message }) => <p className={css.login__error_message}>{message}</p>}
+        />
         <TextField
-          {...register('password')}
-          required
+          {...register('password', { required: t('LogIn.passwordFieldRequiredMessage') })}
           label={t('LogIn.passwordLabel')}
           type="password"
           placeholder={t('LogIn.passwordPlaceholder')}
+        />
+        <ErrorMessage
+          errors={errors}
+          name="password"
+          render={({ message }) => <p className={css.login__error_message}>{message}</p>}
         />
         <Button type="submit">{t('LogIn.signInButton')}</Button>
       </form>
--- app/src/pages/Registration/Registration.tsx
+++ app/src/pages/Registration/Registration.tsx
@@ -3,18 +3,24 @@ import { useDispatch } from 'react-redux';
 import { Button, CircularProgress, TextField, Typography } from '@mui/material';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { ErrorMessage } from '@hookform/error-message';
 import { IUserLogIn, IUserRegistration } from '$types/common';
 import { setToken } from '$store/appSlice';
 import { useSignInMutation, useSignUpMutation } from '$services/api';
 import Section from '$components/Section';
-import { useTranslation } from 'react-i18next';
 import { ROUTES_PATHS } from '$settings/routing';
+import css from './Registration.module.scss';
 
 const Registration: FC = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const dispatch = useDispatch();
-  const { register, handleSubmit } = useForm<IUserRegistration>();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IUserRegistration>();
   const [signUp, { isLoading: isLoadingSignUp }] = useSignUpMutation();
   const [signIn, { isLoading: isLoadingSignIn }] = useSignInMutation();
 
@@ -46,26 +52,40 @@ const Registration: FC = () => {
   return (
     <Section pageAllSpace={true}>
       <Typography variant="h3">{t('Registration.signUpTitle')}</Typography>
-      {/* TODO: add validation */}
       <form onSubmit={handleSubmit(onSubmit)}>
         <TextField
-          {...register('name')}
-          required
+          {...register('name', { required: t('Registration.nameFieldRequiredMessage') })}
           label={t('Registration.nameLabel')}
           placeholder={t('Registration.namePlaceholder')}
         />
+        <ErrorMessage
+          errors={errors}
+          name="name"
+          render={({ message }) => <p className={css.login__error_message}>{message}</p>}
+        />
         <TextField
-          {...register('login')}
-          required
+          {...register('login', { required: t('Registration.loginFieldRequiredMessage') })}
           label={t('Registration.loginLabel')}
           placeholder={t('Registration.loginPlaceholder')}
         />
+        <ErrorMessage
+          errors={errors}
+          name="login"
+          render={({ message }) => <p className={css.login__error_message}>{message}</p>}
+        />
         <TextField
-          {...register('password')}
-          required
+          {...register('password', {
+            required: t('Registration.passwordFieldRequiredMessage'),
+            minLength: { value: 5, message: t('Registration.passwordFieldMinLengthMessage') },
+          })}
           label={t('Registration.passwordLabel')}
           type="password"
           placeholder={t('Registration.passwordPlaceholder')}
+        />
+        <ErrorMessage
+          errors={errors}
+          name="password"
+          render={({ message }) => <p className={css.login__error_message}>{message}</p>}
         />
         <Button type="submit">{t('Registration.signUpButton')}</Button>
       </form>
