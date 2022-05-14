2412a467fa7b488b2583c197768da357c8c5b63a
TarasiukDima <dimatarasiuk91@gmail.com>
2022-05-14 16:41:08 +0300

    feat: add show error for bad request geting boards

--- app/src/pages/Boards/Boards.tsx
+++ app/src/pages/Boards/Boards.tsx
@@ -1,35 +1,39 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useGetAllBoardsQuery } from '$services/api';
-// import { useTranslation } from 'react-i18next';
-// import { useSnackbar } from 'notistack';
+import { useTranslation } from 'react-i18next';
+import { useSnackbar } from 'notistack';
 import Section from '$components/Section';
 import BoardsHead from './BoardsHead';
 import BoardsList from '$components/BoardsList';
 import LightboxNewBoard from '$components/LightboxNewBoard';
-// import CloseButton from '$components/CloseButton';
+import CloseButton from '$components/CloseButton';
 import { IBoard } from '$types/common';
 import css from './Boards.module.scss';
 
 export type TChangeBoardsShow = (searchValue: string) => void;
 
 const Boards: FC = () => {
-  // const { t } = useTranslation();
-  // const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const { t } = useTranslation();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const [showModal, setShowModal] = useState<boolean>(false);
   const [boards, setBoards] = useState<Array<IBoard>>([]);
-  const { data: boardsArray = [] } = useGetAllBoardsQuery();
+  const { data: boardsArray = [], isLoading, isError, isFetching } = useGetAllBoardsQuery();
 
-  // useEffect(() => {
-  //   changeBoardsListShow('');
-  // }, []);
+  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // I think this is not good. But I dont understand ho do it better.
+  // When i change page and get bad request i show 3 Snackbars.
+  useEffect(() => {
+    // console.log('isLoading', isLoading, 'isFetching', isFetching, 'isError', isError);
 
-  // if (!isSuccess) {
-  //   enqueueSnackbar(t('Boards.errorBoardCreat'), {
-  //     variant: 'error',
-  //     autoHideDuration: 3000,
-  //     action: (key) => <CloseButton closeCb={() => closeSnackbar(key)} />,
-  //   });
-  // }
+    if (!isLoading && !isFetching && isError) {
+      enqueueSnackbar(t('Boards.errorGetBoards'), {
+        variant: 'error',
+        autoHideDuration: 3000,
+        action: (key) => <CloseButton closeCb={() => closeSnackbar(key)} />,
+      });
+    }
+  }, [t, closeSnackbar, enqueueSnackbar, isLoading, isFetching, isError]);
+  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   const changeBoardsListShow: TChangeBoardsShow = (searchValue: string) => {
     if (!searchValue.trim().length) {
