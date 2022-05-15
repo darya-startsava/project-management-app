fbb2efee9a7680714fcd045e1c50639b3943ab07
darya-startsava <daryastartsava@gmail.com>
2022-05-15 23:58:01 +0200

    feat: stylize authorization

--- app/src/pages/Authorization/Authorization.tsx
+++ app/src/pages/Authorization/Authorization.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { useDispatch } from 'react-redux';
-import { Button, CircularProgress, TextField, Typography } from '@mui/material';
+import { Button, CircularProgress, TextField, Typography, Grid } from '@mui/material';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -11,6 +11,7 @@ import { useSignInMutation, useSignUpMutation } from '$services/api';
 import Section from '$components/Section';
 import { ROUTES_PATHS } from '$settings/routing';
 import css from './Authorization.module.scss';
+import './Authorization.scss';
 
 interface IAuthorization {
   sortOfAuth: string;
@@ -59,53 +60,63 @@ const Authorization: FC<IAuthorization> = ({ sortOfAuth }) => {
   }
 
   return (
-    <Section pageAllSpace={true}>
-      <Typography variant="h3">{t(`${sortOfAuth}.signTitle`)} </Typography>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {sortOfAuth === 'Registration' && (
-          <>
-            <TextField
-              {...register('name', { required: t('Registration.nameFieldRequiredMessage') })}
-              label={t('Registration.nameLabel')}
-              placeholder={t('Registration.namePlaceholder')}
-            />
-            <ErrorMessage
-              errors={errors}
-              name="name"
-              render={({ message }) => (
-                <p className={css.authorization__error_message}>{message}</p>
-              )}
-            />
-          </>
-        )}
-        <TextField
-          {...register('login', { required: t(`${sortOfAuth}.loginFieldRequiredMessage`) })}
-          label={t(`${sortOfAuth}.loginLabel`)}
-          placeholder={t(`${sortOfAuth}.loginPlaceholder`)}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="login"
-          render={({ message }) => <p className={css.authorization__error_message}>{message}</p>}
-        />
-        <TextField
-          {...register('password', {
-            required: t(`${sortOfAuth}.passwordFieldRequiredMessage`),
-            minLength: {
-              value: passwordValue,
-              message: t('Registration.passwordFieldMinLengthMessage'),
-            },
-          })}
-          label={t(`${sortOfAuth}.passwordLabel`)}
-          type="password"
-          placeholder={t(`${sortOfAuth}.passwordPlaceholder`)}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="password"
-          render={({ message }) => <p className={css.authorization__error_message}>{message}</p>}
-        />
-        <Button type="submit">{t(`${sortOfAuth}.signButton`)}</Button>
+    <Section className="Auth" pageAllSpace={true}>
+      <Typography className="Auth" variant="h3">
+        {t(`${sortOfAuth}.signTitle`)}{' '}
+      </Typography>
+
+      <form className="Auth" onSubmit={handleSubmit(onSubmit)}>
+        <Grid className="Auth" container direction="column">
+          {sortOfAuth === 'Registration' && (
+            <>
+              <TextField
+                className="Auth"
+                {...register('name', { required: t('Registration.nameFieldRequiredMessage') })}
+                label={t('Registration.nameLabel')}
+                placeholder={t('Registration.namePlaceholder')}
+              />
+              <ErrorMessage
+                errors={errors}
+                name="name"
+                render={({ message }) => (
+                  <p className={css.authorization__error_message}>{message}</p>
+                )}
+              />
+            </>
+          )}
+          <TextField
+            className="Auth"
+            {...register('login', { required: t(`${sortOfAuth}.loginFieldRequiredMessage`) })}
+            label={t(`${sortOfAuth}.loginLabel`)}
+            placeholder={t(`${sortOfAuth}.loginPlaceholder`)}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="login"
+            render={({ message }) => <p className={css.authorization__error_message}>{message}</p>}
+          />
+          <TextField
+            className="Auth"
+            {...register('password', {
+              required: t(`${sortOfAuth}.passwordFieldRequiredMessage`),
+              minLength: {
+                value: passwordValue,
+                message: t('Registration.passwordFieldMinLengthMessage'),
+              },
+            })}
+            label={t(`${sortOfAuth}.passwordLabel`)}
+            type="password"
+            placeholder={t(`${sortOfAuth}.passwordPlaceholder`)}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="password"
+            render={({ message }) => <p className={css.authorization__error_message}>{message}</p>}
+          />
+          <Button className="Auth" type="submit" variant="contained">
+            {t(`${sortOfAuth}.signButton`)}
+          </Button>
+        </Grid>
       </form>
       {(isLoadingSignUp || isLoadingSignIn) && <CircularProgress />}
     </Section>
